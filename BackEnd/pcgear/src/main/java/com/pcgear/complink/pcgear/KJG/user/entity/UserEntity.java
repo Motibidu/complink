240d09ad347f0bbf91079dbf05552a3ce77d1ef1
DESKTOP-5L24H2N\golol <jungi77777@gmail.com>
2025-07-30 17:43:43 +0900

    서비스,컨트롤러 제외(ENTITY, DTO, REPOSITORY, SECURITY CONFIG) 설정 완료

---
+++ BackEnd/pcgear/src/main/java/com/pcgear/complink/pcgear/KJG/user/entity/UserEntity.java
@@ -0,0 +1,48 @@
+package com.pcgear.complink.pcgear.KJG.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity(name = "user")
+@Getter
+public class UserEntity extends BaseTimeEntity{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique = true, length = 50, nullable = false)
+	private String email;
+	
+	@Column(unique = true, nullable = false)
+	private String username;
+	
+	@Column(nullable = false)
+	private String password;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private String tel;
+	
+	@Column(nullable = false)
+	private String address;
+	
+	
+	@Builder
+	public UserEntity(String email, String username, String password,String name, String tel, String address) {
+		
+		this.email = email;
+		this.username = username;
+		this.password = password;
+		this.name = name;
+		this.tel = tel;
+		this.address = address;
+	}
+}
---
+++ BackEnd/pcgear/src/main/java/com/pcgear/complink/pcgear/KJG/user/security/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.pcgear.complink.pcgear.KJG.user.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean 
+    public SecurityFilterChain httpFilterChain(HttpSecurity http) throws Exception {  
+        http  
+                .httpBasic(AbstractHttpConfigurer::disable)  
+                .cors(cors ->  
+                        cors.configurationSource(corsConfigurationSource()))  
+                .csrf(AbstractHttpConfigurer::disable)  
+                .formLogin(AbstractHttpConfigurer::disable)  
+                .sessionManagement(sessionManagement -> sessionManagement  
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))  
+                .exceptionHandling(handling -> handling  
+                        .authenticationEntryPoint((request, response, authException) -> {  
+                            response.setStatus(401);  
+                        })  
+                );  
+  
+        return http.build();  
+    }  
+
+    @Bean  
+    public CorsConfigurationSource corsConfigurationSource(){  
+        CorsConfiguration corsConfig = new CorsConfiguration();  
+  
+        corsConfig.addAllowedOrigin("http://localhost:5173");  
+        corsConfig.setAllowCredentials(true);  
+        corsConfig.addAllowedHeader("*");  
+        corsConfig.addAllowedMethod("*");  
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();  
+        source.registerCorsConfiguration("/**", corsConfig);  
+        return source;  
+    } 
+}
