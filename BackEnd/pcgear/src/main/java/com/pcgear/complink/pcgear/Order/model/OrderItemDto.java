333768583bc269899673ee7c9f4e76ae82c3558f
jack981109 <jack981109@gmail.com>
2026-03-10 20:43:01 +0900

    jenkins 빌드 테스트 푸쉬

--- BackEnd/pcgear/src/main/java/com/pcgear/complink/pcgear/Order/model/OrderItemDto.java
+++ BackEnd/pcgear/src/main/java/com/pcgear/complink/pcgear/Order/model/OrderItemDto.java
@@ -7,7 +7,6 @@ import java.math.BigDecimal;
 public class OrderItemDto {
 
         private final Integer orderItemId;
-        private final String itemName;
         private final int quantity;
         private final BigDecimal unitPrice;
         private final BigDecimal totalPrice;
@@ -15,7 +14,6 @@ public class OrderItemDto {
         // OrderItem 엔티티를 DTO로 변환하는 생성자
         public OrderItemDto(OrderItem orderItem) {
                 this.orderItemId = orderItem.getOrderItemId();
-                this.itemName = orderItem.getItemName();
                 this.quantity = orderItem.getQuantity();
                 this.unitPrice = orderItem.getUnitPrice();
                 this.totalPrice = orderItem.getTotalPrice();
